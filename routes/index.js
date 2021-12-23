e592788387f75f46ed9d496a0d83a7535897fd1c
holycho <holylight001@gmail.com>
2021-12-24 02:38:24 +0800

    Add all files

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/account', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM account');
+    const results = { 'results': (result) ? result.rows : null };
+    res.send(JSON.stringify(results));
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
+
+module.exports = router;
